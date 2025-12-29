79bb82e7d7fdc9ef53f28a251c15be03c56c1f75
SanketBhandarii <bhandarisanketp@gmail.com>
2025-12-29 11:36:58 +0530

    Origins changed for the cors

--- backend/main.js
+++ backend/main.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://yoursplanet.vercel.app/'],
+  origin: ['http://localhost:3000', 'https://yoursplanet.vercel.app'],
   credentials: true
 }));
 app.use(cookieParser());
